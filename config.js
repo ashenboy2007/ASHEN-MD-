49b4a955be6bcf5e64e72665c141332afa3bcb8e
ashenboy2007 <hhdinuka99@gmail.com>
2025-09-18 10:43:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+චconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=Psg3CDwS#Zi15x9ADSTtnLszNUWweAuigIsJIJFZJ8Na2-CcyXIQ' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=T9Y3SRpS#NydA0bu1ziuD8314VY6mDI2nDrJeMnMfo7NNn-h7UQs' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
